d90e5b6428e08a7f3ef432b1722a722ee1248cb2
Ayush Jain <ayush.ayushjain12@gmail.com>
2020-10-30 17:04:35 +0530

    Navigation bar implemented

---
+++ navigation-hamburger/script.js
@@ -0,0 +1,18 @@
+let menuBar =  document.getElementById('menu-bars');
+let nav     = document.getElementById('overlay');
+let unoderedList = document.getElementsByTagName('ul'); 
+
+function toogleMenuBar(){
+        [...menuBar.children].forEach(bar => {  
+                    bar.classList.toggle("change");
+            })
+            nav.classList.toggle('active');
+        };
+
+      
+menuBar.addEventListener('click', toogleMenuBar);
+menuBar.addEventListener('keydown', (event)=>{if(event.keyCode === 13){toogleMenuBar()}});
+
+[...unoderedList].forEach(listItem =>{
+    listItem.addEventListener('click',toogleMenuBar );
+})
